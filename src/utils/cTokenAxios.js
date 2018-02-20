470705926acbf8e7bb858d8a398e9d1557ce6eff
waterbeside <waterbeside@163.com>
2018-02-20 13:01:58 +0800

    使用qs格式化post的数据

--- src/utils/cTokenAxios.js
+++ src/utils/cTokenAxios.js
@@ -1,27 +1,45 @@
 import axios from 'axios'
 import config from '../configs'
+import qs from 'qs'
 const tokenAxios = axios.create({
   baseURL: process.env.BASE_API,
   timeout: 10000,
+  transformRequest: [function (data) {
+    // data = Qs.stringify(data);
+    return data;
+  }],
+
 })
 //POST传参序列化(添加请求拦截器)
 tokenAxios.interceptors.request.use(config => {
-    // 在发送请求之前做某件事
-     if(config.method  === 'post'){
-        // JSON 转换为 FormData
-        const formData = new FormData()
-        Object.keys(config.data).forEach(key => formData.append(key, config.data[key]))
-        config.data = formData
-     }
+  // config.headers['Content-Type'] = 'application/json;charset=UTF-8';
+  // 在发送请求之前做某件事
+   if(config.method  === 'post'){
+     config.data = qs.stringify(config.data);
 
-     // config.headers['X-Requested-With'] = 'XMLHttpRequest'
-     // config.headers.common['Content-Type'] = 'application/json;charset=UTF-8'
-     // config.withCredentials = false
-    // 下面会说在什么时候存储 token
-    if (localStorage.getItem('CP_U_TOKEN')) {
-        config.headers.Authorization = 'Bearer ' + localStorage.getItem('CP_U_TOKEN')
-    }
-    return config
+    /*  // JSON 转换为 FormData
+      const formData = new FormData()
+      Object.keys(config.data).forEach(function(value,key,arr){
+        if(typeof(config.data[value])=="object"){
+          Object.keys(config.data[value]).forEach(function(v,k,a){
+            formData.append(value+'['+v+']', config.data[value][v])
+          })
+        }else{
+          formData.append(value, config.data[value])
+        }
+
+      })
+      config.data = formData
+      */
+   }
+
+   // config.headers['X-Requested-With'] = 'XMLHttpRequest'
+   // config.withCredentials = false
+  // 下面会说在什么时候存储 token
+  if (localStorage.getItem('CP_U_TOKEN')) {
+      config.headers.Authorization = 'Bearer ' + localStorage.getItem('CP_U_TOKEN')
+  }
+  return config
 },error =>{
     alert("错误的传参", 'fail')
     return Promise.reject(error)
