715287a99f5bc022bc14626f222b3b2f90025c8b
waterbeside <waterbeside@163.com>
2018-02-22 12:18:42 +0800

    头像组件

---
+++ src/components/CpAvatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <img class="cp-avatar " :src="src" :onerror="'this.src=\''+defaultAvatar+'\';return false'">
+</template>
+
+<script>
+import config from '../configs'
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      defaultAvatar: config.defaultAvatar,
+    }
+  },
+  props: {
+    src:{
+      default: config.defaultAvatar,
+    },
+  },
+  computed: {
+
+  },
+  methods : {
+  }
+}
+</script>
