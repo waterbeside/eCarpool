d7b1e342bf2a35c455d746d4d80ac67e40772403
waterbeside <waterbeside@163.com>
2018-07-03 11:56:40 +0800

    chore: update

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -12,9 +12,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : require('../config/prod.env')
+const env = process.env.NODE_ENV === 'testing' ? require('../config/test.env') : require('../config/prod.env');
 // const env = config.build.env
 
 const webpackConfig = merge(baseWebpackConfig, {
