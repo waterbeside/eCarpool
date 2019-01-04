0e19e154ba5ad6953cac75ab192d15ee513d0373
waterbeside <waterbeside@163.com>
2019-01-04 16:20:55 +0800

    feat: 用户名片组件

---
+++ src/modules/index/components/CpNameCard.vue
@@ -0,0 +1,45 @@
+<template>
+   <div class="cp-name-card">
+     <slot name="before"></slot>
+     <div class="cp-avatar-wrapper">
+       <cp-avatar :src="avatar"></cp-avatar>
+     </div>
+     <div class="cp-mainbox">
+       <slot name="before-content"></slot>
+       <div class="cp-content">
+         <div class="name">{{name}}</div>
+         <div class="department">{{department}}</div>
+         <slot name="more"></slot>
+       </div>
+       <slot name="after-content"></slot>
+     </div>
+     <slot name="after"></slot>
+  </div>
+</template>
+
+<script>
+import CpAvatar from '@/components/CpAvatar'
+
+export default {
+  components: {
+    CpAvatar
+  },
+  data () {
+    return {
+
+    }
+  },
+  props: {
+
+    name : String,
+    avatar : String,
+    department : String,
+  },
+  computed: {
+
+  },
+  methods : {
+
+  },
+}
+</script>
